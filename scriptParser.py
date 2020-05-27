d98a9295d4cb1cc569e8473e5110db451b779be9
howobee <howobee@gmail.com>
2020-05-27 20:41:54 +0300

    second commit

--- scriptParser.py
+++ scriptParser.py
@@ -2,7 +2,49 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import csv
-import time
+import os
+from selenium import webdriver
+from time import sleep
+import pytesseract
+from PIL import Image
+
+#Функция для считывания телефонного номера 
+def avito_get_phonenum(url):
+    
+    #Открываем браузер 
+    browser = webdriver.Firefox(executable_path=r'./geckodriver.exe')
+    browser.get(url)
+    
+    #Находим кнопку и кликаем
+    button= browser.find_element_by_xpath('//a[@class="button item-phone-button js-item-phone-button button-origin contactBar_greenColor button-origin_full-width button-origin_large-extra item-phone-button_hide-phone item-phone-button_card js-item-phone-button_card contactBar_height"]')
+    button.click()
+    sleep(3)
+    
+    #Сохраняем скриншот дисплее браузера в файл "avito_scr.png"
+    browser.save_screenshot('avito_scr.png')
+    image_s_l = browser.find_element_by_xpath('//div[@class="item-phone-big-number js-item-phone-big-number"]//*')
+    
+    #Берем координаты начальной точки и размеры картинки для crop()
+    location = image_s_l.location     # dict{x:value, y:value}
+    size = image_s_l.size             # dict{width:value, height:value}
+
+    # Загружаем картинку и вызываем функцию crop() для обрезки картинки
+    image = Image.open('avito_scr.png')
+    x = location['x']
+    y = location['y']
+    width = size['width']
+    height = size['height']
+    image.crop((x, y, x+width, y+height)).save('tel_img.gif')
+
+    # Закрываем браузер
+    browser.close()
+
+    #Загружаем скриншот и распознаем номер на картинки в текст
+    image_phone_number = Image.open('tel_img.gif')
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    phone_number =  pytesseract.image_to_string(image_phone_number)
+    #Функция вовращает номер телефона строкой
+    return phone_number
 
 # Фукнция для записи данных в файл в csv формате
 def write_csv(data):
@@ -11,7 +53,8 @@ def write_csv(data):
         writer.writerow((data['title'],
                         data['price'],
                         data['metro'],
-                        data['url']))
+                        data['url'],
+                        data['phone_number']))
 
 
 #Функция для парсинга html и создания данных
@@ -39,13 +82,21 @@ def get_page_data(html):
 
         except:
             metro=''
-
+        phone_number = avito_get_phonenum(url)
+        
         data = {'title': title,
                 'price': price,
                 'metro': metro,
-                'url': url}
+                'url': url,
+                'phone_number': phone_number}
         
         write_csv(data)
+
+        #Удаляем картинки из папки
+        file1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'avito_scr.png')
+        file2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tel_img.gif')
+        os.remove(file1)
+        os.remove(file2)
 #Функция для cocтавления HTTP запроса и получения ответа от сервера
 def get_html(url):
     res = requests.get(url)    
@@ -62,12 +113,12 @@ def get_total_pages(html):
 #Главная функция
 def main():
     
-    url='https://www.avito.ru/moskva/noutbuki?p=1&q=asus'
-    base_url='https://www.avito.ru/moskva/noutbuki?'
-    page_part='p='
-    query_part = '&q=asus'
+    base_url  = 'https://www.avito.ru/moskva/avtomobili?'
+    page_part ='p='
+    query_part ='&q=bmw'
+    url='https://www.avito.ru/moskva/avtomobili?p=1&q=bmw'
     total_pages = get_total_pages(get_html(url))
-    for i in range(1, total_pages):
+    for i in range(1, 2):
         url_gen = base_url+page_part+str(i)+query_part
         html = get_html(url_gen)
         get_page_data(html)
